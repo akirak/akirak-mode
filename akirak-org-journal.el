9abadec1a3f2066feb8d15df05bbf7bcdb206f2d
Akira Komamura <akira.komamura@gmail.com>
2021-11-12 13:49:58 +0900

    feat(org-journal): init

---
+++ akirak-org-journal.el
@@ -0,0 +1,88 @@
+;;; akirak-org-journal.el --- Utilities for org-journal -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "26.1") (org "9.4") (org-journal "2.1"))
+;; Keywords: outlines
+;; URL: https://github.com/akirak/trivial-elisps
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a collection of helper libraries for Org mode.
+
+;;; Code:
+
+(require 'org)
+(require 'org-journal)
+
+;;;###autoload
+(defun akirak-org-journal-find-location (&optional time)
+  "Go to the beginning of the journal file.
+
+This can be used for defining an `org-capture' template to create
+an entry in the journal.
+
+Optionally, you can specify TIME for pointoing to a particular
+date, which is given to `org-journal-new-entry'."
+  ;; Open today's journal, but specify a non-nil prefix argument in order to
+  ;; inhibit inserting the heading; org-capture will insert the heading.
+  (org-journal-new-entry t time)
+  ;; Position point on the journal's top-level heading so that org-capture
+  ;; will add the new entry as a child entry.
+  (widen)
+  (re-search-backward (rx bol "* "))
+  (org-show-entry))
+
+;;;###autoload
+(cl-defun akirak-org-journal-ensure-group (heading &key tags)
+  "Go to a particular group in the journal.
+
+This moves the point to a group with a HEADING. If the heading
+does not , it is created.
+
+Optionally, you can specify TAGS which is added to the heading."
+  (declare (indent 1))
+  (akirak-org-journal-find-location)
+  (let ((end (save-excursion
+               (org-end-of-subtree 'invisible))))
+    (if (re-search-forward (rx-to-string `(and bol "**" (+ space) ,heading))
+                           end t)
+        (end-of-line)
+      (goto-char end)
+      (progn
+        (just-one-space -1)
+        (delete-horizontal-space)
+        (insert "\n** " heading))
+      (when tags
+        (org-set-tags tags))
+      (org-end-of-line))))
+
+(cl-defmacro akirak-org-journal-group-target (heading &key tags)
+  "Return a function that ensures a journal group.
+
+For HEADING and TAGS, see `akirak-org-journal-ensure-group'."
+  (declare (indent 1))
+  `(lambda () (akirak-org-journal-ensure-group ,heading
+                :tags ,tags)))
+
+(provide 'akirak-org-journal)
+;;; akirak-org-journal.el ends here
