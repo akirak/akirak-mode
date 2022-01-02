4267498e450b3525835c2a643f759d57c1c2c529
Akira Komamura <akira.komamura@gmail.com>
2022-01-02 15:01:18 +0900

    feat(org): Add akirak-org-sort-buffer-mode

---
+++ akirak-org.el
@@ -0,0 +1,58 @@
+;;; akirak-org.el --- A collection of helpers for Org -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; URL: https://github.com/akirak/trivial-elisps
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This library provides convenience functions for `org-mode'.
+
+;;; Code:
+
+(require 'org)
+
+;;;###autoload
+(defun akirak-org-sort-buffer ()
+  "Sort entries in the buffer according to sorting_type property values."
+  (interactive)
+  (org-with-wide-buffer
+   (goto-char (point-min))
+   (while (re-search-forward (org-re-property "sorting_type") nil t)
+     (let ((line (thing-at-point 'line t)))
+       (if (string-match org-property-re line)
+           (org-sort-entries nil
+                             (thread-last (match-string 3 line)
+                               (string-to-list)
+                               (car)))
+         (error "Property didn't match")))
+     (goto-char (org-entry-end-position)))))
+
+(define-minor-mode akirak-org-sort-buffer-mode
+  "Sort entries in the buffer before save."
+  nil nil nil
+  (if akirak-org-sort-buffer-mode
+      (add-hook 'before-save-hook #'akirak-org-sort-buffer nil t)
+    (remove-hook 'before-save-hook #'akirak-org-sort-buffer t)))
+
+(provide 'akirak-org)
+;;; akirak-org.el ends here
