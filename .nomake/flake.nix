bd31e0a24f774147c128076991d18ff08590ea2b
Akira Komamura <akira.komamura@gmail.com>
2022-02-16 10:56:58 +0900

    chore: Init nomake

---
+++ .nomake/flake.nix
@@ -0,0 +1,55 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    akirak = {
+      flake = false;
+      owner = "akirak";
+      repo = "trivial-elisps";
+      type = "github";
+    };
+    avy = {
+      flake = false;
+      owner = "abo-abo";
+      repo = "avy";
+      type = "github";
+    };
+    dash = {
+      flake = false;
+      owner = "magnars";
+      repo = "dash.el";
+      type = "github";
+    };
+    "f" = {
+      flake = false;
+      owner = "rejeep";
+      repo = "f.el";
+      type = "github";
+    };
+    memoize = {
+      flake = false;
+      owner = "skeeto";
+      repo = "emacs-memoize";
+      type = "github";
+    };
+    org-journal = {
+      flake = false;
+      owner = "bastibe";
+      repo = "org-journal";
+      type = "github";
+    };
+    org-starter = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-starter";
+      type = "github";
+    };
+    "s" = {
+      flake = false;
+      owner = "magnars";
+      repo = "s.el";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
