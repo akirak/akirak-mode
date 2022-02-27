00931e09439db10b4d7e1fc7e734bf3ff7ba5012
Akira Komamura <akira.komamura@gmail.com>
2022-02-27 15:48:53 +0900

    feat(elec-pair): Add akirak-elec-pair-wrap command

--- akirak-elec-pair.el
+++ akirak-elec-pair.el
@@ -31,6 +31,28 @@
 
 (require 'elec-pair)
 
+(defcustom akirak-elec-pair-special-chars
+  (string-to-list "%$#")
+  "List of characters indicating an orientation of interpolation."
+  :type '(repeat character))
+
+(defun akirak-elec-pair--new-bracket-pair ()
+  "Get the new bracket pairs from the user."
+  (let* ((open (read-char (format "Open paren (or %s for interpolation): "
+                                  (mapconcat #'char-to-string
+                                             akirak-elec-pair-special-chars
+                                             ""))))
+         (specialp (memq open akirak-elec-pair-special-chars))
+         (open-char (if specialp
+                        (read-char "Open paren: ")
+                      open))
+         (close-char (or (nth 1 (electric-pair-syntax-info open-char))
+                         (alist-get open-char '((?\{ . ?\})))
+                         (error "Cannot find syntax info for %c" open-char))))
+    (list open-char
+          close-char
+          (when specialp (char-to-string open)))))
+
 ;;;###autoload
 (defun akirak-elec-pair-replace (c)
   "Replace a pair of parentheses/brackets of C around the point."
@@ -78,6 +100,29 @@
         (delete-char 1)
         (goto-char (1- end))
         (backward-delete-char 1)))))
+
+;;;###autoload
+(defun akirak-elec-pair-wrap ()
+  "Wrap the current sexp or region with a pair."
+  (interactive)
+  (pcase-let* ((`(,start . ,end) (if (region-active-p)
+                                     (cons (region-beginning) (region-end))
+                                   (bounds-of-thing-at-point 'sexp)))
+               (overlay (make-overlay start end))
+               (`(,open-char ,close-char ,prefix)
+                (progn
+                  (overlay-put overlay 'face 'highlight)
+                  (akirak-elec-pair--new-bracket-pair))))
+    (save-excursion
+      (delete-overlay overlay)
+      (goto-char start)
+      (when prefix
+        (insert prefix))
+      (insert-char open-char)
+      (goto-char (if prefix
+                     (+ (length prefix) end 1)
+                   (1+ end)))
+      (insert-char close-char))))
 
 (provide 'akirak-elec-pair)
 ;;; akirak-elec-pair.el ends here
