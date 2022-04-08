fce4896d7ae196a8af4c0b0f5af9df9eb6c2e9d6
Akira Komamura <akira.komamura@gmail.com>
2022-04-08 11:37:24 +0900

    feat(pocket-reader): Add cleanup function

---
+++ akirak-pocket-reader.el
@@ -0,0 +1,16 @@
+;;; akirak-pocket-reader.el ---  -*- lexical-binding: t -*-
+
+;;;###autoload
+(defun akirak-pocket-reader-cleanup ()
+  "Erase unwanted entries from the Pocket account."
+  (interactive)
+  (goto-char (point-min))
+  (let (ent)
+    (while (setq ent (tabulated-list-get-entry))
+      (when (equal (elt ent 3) "mobile.twitter.com")
+        (pocket-reader--delete-items (pocket-reader--current-item)))
+      (forward-line))
+    (pocket-reader-refresh)))
+
+(provide 'akirak-pocket-reader)
+;;; akirak-pocket-reader.el ends here
